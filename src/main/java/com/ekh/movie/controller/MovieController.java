2457185fc2f8629209a5f07e73e3aae9bf99b21a
조준희 <soyre1928@gmail.com>
2025-04-03 10:04:12 +0900

    db에 데이터 추가

--- src/main/java/com/ekh/movie/controller/MovieController.java
+++ src/main/java/com/ekh/movie/controller/MovieController.java
@@ -6,21 +6,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ekh.movie.openapi.DTO.MovieDTO;
+import com.ekh.movie.openapi.DTO.ResultDTO;
 import com.ekh.movie.service.MovieService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 
+import lombok.RequiredArgsConstructor;
+
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api")
 public class MovieController {
 	
 	private final MovieService movieService;
 	
-	public MovieController(MovieService movieService) {
-        this.movieService = movieService;
-    }
 	
 	@GetMapping("/movie/search/{qurey}")
-	public MovieDTO save(@PathVariable(value = "qurey") String qurey) throws JsonProcessingException {
-		return movieService.save(qurey);
+	public void save(@PathVariable(value = "qurey") String qurey) throws JsonProcessingException {
+		movieService.save(qurey);
 	}
+
 }
--- src/main/java/com/ekh/movie/service/MovieService.java
+++ src/main/java/com/ekh/movie/service/MovieService.java
@@ -1,6 +1,10 @@
 package com.ekh.movie.service;
 
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -11,6 +15,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import com.ekh.movie.openapi.DAO.ResultDAO;
 import com.ekh.movie.openapi.DTO.MovieDTO;
 import com.ekh.movie.openapi.DTO.ResultDTO;
 import com.ekh.movie.repository.ResultRepository;
@@ -28,7 +33,7 @@ public class MovieService {
     private final ResultRepository resultRepository;
 
 
-	public MovieDTO save(String qurey) throws JsonProcessingException {
+	public void save(String qurey) throws JsonProcessingException {
 		
 		RestTemplate restTemplate = new RestTemplate();
 
@@ -50,8 +55,54 @@ public class MovieService {
         System.out.println(response.getBody());
 
         MovieDTO movieDTO = response.getBody();
-        return movieDTO;
+        
+        if (movieDTO != null && movieDTO.getResults() != null) {
+            List<ResultDAO> resultDAOs = new ArrayList<>();
+            
+            for (ResultDTO dto : movieDTO.getResults()) {
+                ResultDAO dao = new ResultDAO();
+                
+                dao.setAdult(dto.isAdult());
+                dao.setBackdrop_path(dto.getBackdrop_path());
+                dao.setId(dto.getId());
+                dao.setOriginal_language(dto.getOriginal_language());
+                dao.setOriginal_title(dto.getOriginal_title());
+                dao.setOverview(dto.getOverview());
+                dao.setPopularity(dto.getPopularity());
+                dao.setPoster_path(dto.getPoster_path());
+                dao.setRelease_date(dto.getRelease_date());
+                dao.setTitle(dto.getTitle());
+                dao.setVideo(dto.isVideo());
+                dao.setVote_average(dto.getVote_average());
+                dao.setVote_count(dto.getVote_count());
+                
+                resultDAOs.add(dao);
+            }
+            
+            resultRepository.saveAll(resultDAOs);
+        }
          
 	}
+	
+	public ResultDAO resultSave(ResultDTO resultDTO) {
+		ResultDAO resultDAO = new ResultDAO();
+
+	    resultDAO.setAdult(resultDTO.isAdult());
+	    resultDAO.setBackdrop_path(resultDTO.getBackdrop_path());
+	    resultDAO.setId(resultDTO.getId());
+	    resultDAO.setOriginal_language(resultDTO.getOriginal_language());
+	    resultDAO.setOriginal_title(resultDTO.getOriginal_title());
+	    resultDAO.setOverview(resultDTO.getOverview());
+	    resultDAO.setPopularity(resultDTO.getPopularity());
+	    resultDAO.setPoster_path(resultDTO.getPoster_path());
+	    resultDAO.setRelease_date(resultDTO.getRelease_date());
+	    resultDAO.setTitle(resultDTO.getTitle());
+	    resultDAO.setVideo(resultDTO.isVideo());
+	    resultDAO.setVote_average(resultDTO.getVote_average());
+	    resultDAO.setVote_count(resultDTO.getVote_count());
+	    
+	    return resultDAO;
+	}
+	
 
 }
